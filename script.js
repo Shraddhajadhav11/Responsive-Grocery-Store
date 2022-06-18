5d075b64b367e544424a48f2abf9de672c866699
Shraddhajadhav11 <shraddhaj430@gmail.com>
2022-06-18 18:05:37 +0530

    first commit

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.querySelector('#menu-btn').onclick = () =>{
 }
 
 // Dark Mode
-let darkmode = document.querySelector('#darkmode');
+/*let darkmode = document.querySelector('#darkmode');
  
 darkmode.onclick = () => {
     if(darkmode.classList.contains('bx-moon')){
@@ -45,7 +45,7 @@ darkmode.onclick = () => {
         darkmode.classList.replace('bx-sun','bx-moon');
         document.body.classList.remove('active');
     }
-}
+}*/
 
 window.onscroll = () =>{
     searchForm.classList.remove('active');
@@ -95,3 +95,36 @@ var swiper = new Swiper(".review-slider", {
       },
     },
 });
+
+//Timer
+let countDate = new Date('june 1 2023 00:00:00').getTime();
+
+function CountDown(){
+
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function(){
+    CountDown();
+},1000)
+
+
+
+
